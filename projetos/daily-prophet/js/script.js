186258dd14a643fd152605089dce51891d2b01de
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-06 02:41:58 -0300

    Projeto Final

--- projetos/daily-prophet/js/script.js
+++ projetos/daily-prophet/js/script.js
@@ -2,6 +2,11 @@ const menuAnimado = document.querySelector('.animated-menu');
 const navBar = document.querySelector('.navbar-ulist');
 const divBroom = document.querySelector('.broom-img');
 const divAnuncio = document.querySelector('.mr-1e');
+const underImg = document.querySelector('.under-img');
+const containerLeads = document.querySelector('.container-leads');
+const containerMain = document.querySelector('.container-main');
+
+const arrTextos = [underImg, navBar, containerLeads, containerMain];
 
 function clicaMenu() {
   menuAnimado.classList.toggle('change');
@@ -15,3 +20,13 @@ function clicaBroom() {
 }
 
 divAnuncio.addEventListener('click', clicaBroom);
+
+function classNoTexto() {
+  event.target.classList.toggle('text-animation');
+}
+
+function clicaTexto(item) {
+  item.addEventListener('click', classNoTexto);
+}
+
+arrTextos.forEach(clicaTexto);
