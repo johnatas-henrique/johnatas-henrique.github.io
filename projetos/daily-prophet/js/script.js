1a35c6e34ae139ab471fcfb594674ed12c946339
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-01-05 22:59:58 -0300

    Ver no celular - noite

--- projetos/daily-prophet/js/script.js
+++ projetos/daily-prophet/js/script.js
@@ -1,9 +1,17 @@
 const menuAnimado = document.querySelector('.animated-menu');
 const navBar = document.querySelector('.navbar-ulist');
+const divBroom = document.querySelector('.broom-img');
+const divAnuncio = document.querySelector('.mr-1e');
 
 function clicaMenu() {
-    menuAnimado.classList.toggle('change');
-    navBar.classList.toggle('hide');
-  }
+  menuAnimado.classList.toggle('change');
+  navBar.classList.toggle('hide');
+}
 
 menuAnimado.addEventListener('click', clicaMenu);
+
+function clicaBroom() {
+  divBroom.classList.add('broom-animation');
+}
+
+divAnuncio.addEventListener('click', clicaBroom);
