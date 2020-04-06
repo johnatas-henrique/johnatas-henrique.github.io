86f0b19f253508152c982ea7292d1d86f9d0bfd0
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-04-06 19:03:44 -0300

    Exercise 2-2

--- exercises/18_3/use-effect-testing/src/Greeting.js
+++ exercises/18_3/use-effect-testing/src/Greeting.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Greeting = () => {
-  const [nome, setNome] = useState('');
+  const locStorParse = () => {
+    return localStorage.getItem('nome') || '';
+  }
+  const [nome, setNome] = useState(locStorParse);
+
+  useEffect(() => {
+    localStorage.setItem('nome', nome);
+  }, [nome]);
 
   return (
     <div style={{ border: 1 + "px solid black" }}>
       <h1>Greeting Area</h1>
       <div>
-        <input 
+        <input
           type="text"
           onChange={((e) => setNome(e.target.value))}
           value={nome}
         />
-        <h2>Greetings, {nome}.</h2>
+        {nome ? <p>Greetings {nome}</p> : <p>Escreva um nome na caixa</p>}
       </div>
     </div>
   )
