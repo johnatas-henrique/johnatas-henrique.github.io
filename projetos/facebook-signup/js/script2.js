b70d41d9501c1e4f06c2a49b9f8e469e23a1b3ef
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-08 17:24:33 -0300

    cleaning up and starting React portfolio

--- projetos/facebook-signup/js/script2.js
+++
@@ -1,7 +0,0 @@
-function verificaSelectVazia() {
-  if (valorGenero === 3 && selectPronome.value === '') {
-    contadorValida += 1;
-    alertaErro += '\nO campo Selecione seu pronome está vazio.';
-  }
-}
-botaoEnviar.addEventListener('click', verificaSelectVazia);
