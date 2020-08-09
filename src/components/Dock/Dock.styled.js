8e9d5e94887f41c2670d971884ee58a6c747393c
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-09 04:03:12 -0300

    Adding animations to Dock

--- src/components/Dock/Dock.styled.js
+++ src/components/Dock/Dock.styled.js
@@ -1,8 +1,30 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const showText = keyframes`
+  0% {
+    opacity: 0.1;
+  }
+  100% {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+`;
+
+const iconRotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(-180deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+`;
 
 const StyledDock = styled.nav`
-
   align-items: center;
+  animation: ${showText} 1s linear;
   display: flex;
   justify-content: space-around;
   overflow: hidden;
@@ -30,23 +52,28 @@ const StyledDock = styled.nav`
   }
 
   img {
+    animation: ${iconRotate} 1s linear;
     height: 2rem;
     margin-top: 4px;
+    transform: rotate(360deg);
     @media (max-width: ${({ theme }) => theme.mobile}) {
       height: 1.5rem;
       text-align: center;
     }
   }
+  
+  img:hover {
+    animation: ${iconRotate} 3s linear infinite;
+  }
 
   p, a {
+    animation: ${iconRotate} 1s linear;
     margin: 0 1rem;
     font-size: 2rem;
-    /* text-transform: uppercase; */
     font-weight: bold;
     color: ${({ theme }) => theme.primaryTwo};
     text-decoration: none;
     transition: color 0.3s linear;
-    
     @media (max-width: ${({ theme }) => theme.mobile}) {
       font-size: 1.5rem;
       text-align: center;
