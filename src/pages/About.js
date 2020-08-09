8406bc9c714e3ed443ea77fe5d006300268a4186
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-09 20:19:33 -0300

    Making About Me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { PortfolioContext } from '../context/Portfolio';
+import StyledAbout from './About.styled';
+import aboutImg from '../images/aboutme.jpeg';
 
 const About = () => {
   const { setLocation } = useContext(PortfolioContext);
@@ -9,10 +11,30 @@ const About = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Hello. This is burger menu tutorial</h1>
-      <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
-    </div>
+    <StyledAbout>
+      <div className="text">
+        <h1>JOHNATAS HENRIQUE</h1>
+        <h3>
+          Estudando para me tornar um desenvolvedor de software full-stack,
+          adoro trabalhar em equipe e compartilhar conhecimentos. Estou fazendo uma
+          transição de carreira, trabalhei por anos em outra área de tecnologia,
+          a de suporte a TI.
+        </h3>
+        <h3>
+          Acredito firmemente que pessoas são a parte mais importante
+          para alcançar o sucesso, por isso trabalho fortemente tanto
+          para melhorar meu conhecimento técnico, quanto minhas habilidades
+          de comunicação interpessoal.
+        </h3>
+        <h3>
+          Tenho a certeza de que trabalho em grupo, com foco na qualidade,
+          sempre vão alcançar os melhores resultados.
+        </h3>
+      </div>
+      <div className="image">
+        <img src={aboutImg} alt="Minha foto" />
+      </div>
+    </StyledAbout>
   );
 };
 
---
+++ src/pages/About.styled.js
@@ -0,0 +1,89 @@
+import styled, { keyframes } from 'styled-components';
+
+const showText = keyframes`
+  0% { 
+    opacity: 0.1;
+  }
+  70% {
+    transform: translateY(-10%);
+  }
+  100% {
+    transform: translateY(0%);
+    opacity: 1;
+  }
+`;
+
+const StyledAbout = styled.div`
+
+  align-items: center;
+  animation: ${showText} 1s linear;
+  display: flex;
+  margin: 0 auto;
+  width: 80%;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    display: block;
+    font-size: 1.5rem;
+    text-align: center;
+    width: 100%;
+  }
+
+  div.text{
+    margin-right: 40px;
+    width: 60%;
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+      margin-right: 0;
+      align-items: flex-start;
+      margin-bottom: 4rem;
+      width: 100%;
+    }
+  }
+
+  .image {
+    border-radius: 12%;
+    display: inline-block;
+    position: relative;
+    width: 40%;
+    @media (max-width: ${({ theme }) => theme.mobile}) {
+      display: none;
+    }
+}
+  .image img {
+      border-radius: 12%;
+      display: block;
+      max-height:100%;
+      max-width: 100%;
+  }
+  .image:after {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 100%;
+      background: radial-gradient(
+        ellipse at center, 
+        rgba(255,255,255,0) 0%,
+        rgba(255,255,255,0) 72%,
+        rgba(255,255,255,1) 100%
+      );
+      border-radius: 12%;
+      position: absolute;
+      top: 0; left: 0;
+      @media (max-width: ${({ theme }) => theme.mobile}) {
+      display: none;
+    }
+  }
+
+  h1, h2 {
+    color: ${({ theme }) => theme.primaryOne};
+    font-size: 200%;
+    padding: 0 1rem;
+  }
+
+  h3, h4 {
+    color: ${({ theme }) => theme.primaryOne};
+    padding: 0 1rem;
+    text-align: justify;
+  }
+`;
+
+export default StyledAbout;
