7867d6c19759aa75473630ae464306845de4414b
Johnatas Henrique <johnatas.henrique@gmail.com>
2022-03-04 13:20:32 -0300

    feat: adds profile image

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
-import { Container, Box, Heading } from '@chakra-ui/react';
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react';
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius='lg' bg='red' p={3} mb={6} align='center'>
+      <Box
+        borderRadius='lg'
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        textAlign='center'
+      >
         Olá, eu sou um desenvolvedor full-stack brasileiro!
       </Box>
-      <Box display={{md: 'flex'}}>
+      <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as='h2' variant='page-title'>
             Johnatas Henrique
           </Heading>
           <p>Desenvolvedor Web</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+        <Image
+          borderColor='whiteAlpha.800'
+          borderWidth={2}
+          borderStyle='solid'
+          maxWidth='100px'
+          display='inline-block'
+          borderRadius='full'
+          src='/images/takuya.jpg'
+          alt='Profile Image'
+        />
         </Box>
       </Box>
     </Container>
