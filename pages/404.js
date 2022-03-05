9932eefc903aeec196a1b3f68a9fe35a9e773a69
Johnatas Henrique <johnatas.henrique@gmail.com>
2022-03-05 01:52:19 -0300

    feat: adds a 404 page

---
+++ pages/404.js
@@ -0,0 +1,36 @@
+import NextLink from 'next/link';
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button,
+  Image
+} from '@chakra-ui/react';
+import Layout from '../components/layouts/article';
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <Container>
+        <Heading as="h1">Não encontrada</Heading>
+        <Text>A página que você procura não foi encontrada.</Text>
+        <Image
+          display='inline-block'
+          src='/images/caveman.gif'
+          alt='Profile Image'
+        />
+        <Divider my={6} />
+
+        <Box my={6} align="center">
+          <NextLink href="/" passHref>
+            <Button colorScheme="teal">Voltar ao início</Button>
+          </NextLink>
+        </Box>
+      </Container>
+    </Layout>
+  )
+}
+
+export default NotFound;
